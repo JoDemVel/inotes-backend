5ad1a8b3186d79bb5b51a190c6acddbd31eed4e4
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2025-01-26 18:52:29 -0400

    init repo

---
+++ src/test/java/com/app/dharbor/inotes/InotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dharbor.inotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
